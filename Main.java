c0a83dab70f3b359fe361bc4feabe385f5b584e8
ajaychowdarijava-create <ajaychowdari.java@gmail.com>
2025-09-01 16:38:23 -0700

    #changed to "O" to "o"

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello WOrld");
+    public static void main(String[] args)
+    {
+        System.out.println("Hello W0rld");
     }
 }
